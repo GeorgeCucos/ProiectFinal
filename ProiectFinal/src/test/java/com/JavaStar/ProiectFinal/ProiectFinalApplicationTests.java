b314a3989b2290ac345b26e4862a9fbf8743c4a7
GeorgeCucos <129759476+GeorgeCucos@users.noreply.github.com>
2023-06-17 21:16:40 +0300

    Add files via upload

---
+++ ProiectFinal/src/test/java/com/JavaStar/ProiectFinal/ProiectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaStar.ProiectFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
