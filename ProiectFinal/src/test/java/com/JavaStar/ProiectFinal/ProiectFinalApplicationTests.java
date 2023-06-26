9aaea8c6ec59e85259ef0659727f80ac63dd954d
GeorgeCucos <cucos.george3@gmail.com>
2023-06-26 17:37:40 +0300

    entitati si mapare

--- ProiectFinal/src/test/java/com/JavaStar/ProiectFinal/ProiectFinalApplicationTests.java
+++ ProiectFinal/src/test/java/com/JavaStar/ProiectFinal/ProiectFinalApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProiectFinalApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+		System.out.println(1);
 	}
 
 }
